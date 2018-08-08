9aca551db4f48a850a1d4bb36935e43906f590ae
jooeun <rlawndms222@naver.com>
2018-08-08 18:10:47 +0900

    algorithm1

---
+++ algorithm1.java
@@ -0,0 +1,29 @@
+package algorithm1;
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(arr[0]);
+        
+        for(int i=1; i<arr.length; i++) {
+        	if(arr[i] != arr[i-1]) {
+        		list.add(arr[i]);
+        	}
+        }
+        
+        int[] answer= new int[list.size()];
+        for(int i=0; i<list.size(); i++)
+        	answer[i]=list.get(i);
+        
+        return answer;
+    }
+    
+    public static void main(String[] args) {
+    	Solution test = new Solution();
+    	int []arr = {1,1,3,3,0,1,1};
+    	for(int i:test.solution(arr)) {
+    		System.out.print(i);
+    	}
+	}
+}
